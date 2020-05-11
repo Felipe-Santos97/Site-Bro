43a6fba2322d6068f6889186ee2bd84ab1c08b47
Felipe Santos Costa <felipe.pier7@gmail.com>
2020-05-10 23:36:07 -0300

    login done

--- site/paginas/login.js
+++ site/paginas/login.js
@@ -12,7 +12,7 @@ btn_cadastro.addEventListener('click' , cadastrar)
 
 function cadastrar(){
         if(email_cadastro.value !== '' && nome.value !== '' && senha_cadastro.value !== ''){
-            mensagem.innerHTML = `<p>Obrigado <strong>${nome.value}</strong> por se cadastrar.</p>`
+            mensagem.innerHTML = `<p>Muito bem <strong>${nome.value}</strong>, agora faça o login !</p>`
         }else{
             window.alert('Coloque seus dados !')
       }
@@ -27,9 +27,22 @@ var btn = document.querySelector('#btn-login')
 btn.addEventListener('click', login)
 
 function login(){  
-        if(email.value !== '' && senha.value !== ''){
-            alert('logado')
-        }else{
-            alert('nao logado')
+        if(email.value === '' && senha.value === ''){
+            alert('Digite seus dados')
+        }else if(email_cadastro.value == email.value && senha_cadastro.value == senha.value){
+                msg_login.innerHTML = `<p>Conta logada com sucesso !</p>` 
+                mensagem.innerHTML = `<p><strong>Obrigado ${nome.value} por se cadastrar, o sistema ainda esta em produção <br> e futuramente seus dados serão salvos.</strong></p>`
+             }else{
+                 alert('Usuario não cadastrado !')
+             }
+            
         }
-}
+
+        // ESQUECEU A CONTA
+        var esqueceu_conta = document.getElementById('esqueceu')
+        esqueceu_conta.addEventListener('click', msg_recuperar)
+
+        function msg_recuperar(){
+            alert('Vishe, melhor fazer outra conta')
+        }
+
